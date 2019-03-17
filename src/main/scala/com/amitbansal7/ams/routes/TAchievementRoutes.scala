28e973ea302e0b940a83b3a82daa82b0a39238cc
Amit Bansal <007amitbansal@gmail.com>
2019-03-17 21:38:27 +0530

    added comments in routes

--- src/main/scala/com/amitbansal7/ams/routes/TAchievementRoutes.scala
+++ src/main/scala/com/amitbansal7/ams/routes/TAchievementRoutes.scala
@@ -9,22 +9,28 @@ import com.amitbansal.ams.config.JsonSupport._
 class TAchievementRoutes(tAchievementService: TAchievementService) {
   def route: Route = {
     pathPrefix("tachievements") {
+
+      //Get all achievement by userid
       (path("allUserid") & get) {
         parameter('userId) { (userId) =>
           complete(tAchievementService.getAllForUserId(userId))
         }
+        //get all aggregated(counted) data
       } ~ (path("allagg") & get) {
         parameter('fromDate.?, 'toDate.?) { (fromDate, toDate) =>
           complete(tAchievementService.getAllAggregated(fromDate, toDate))
         }
+        //get all users with their achievements, optional filters: fromDate, toDate, department
       } ~ (path("all") & get) {
         parameter('fromDate.?, 'toDate.?, 'department.?) { (fromDate, toDate, department) =>
           complete(tAchievementService.getAll(fromDate, toDate, department))
         }
+        //Delete an achievement with achievement id and valid token
       } ~ (path("delete") & delete) {
         parameter('id, 'token) { (id, token) =>
           complete(tAchievementService.deleteOne(id, token))
         }
+        //update an achievement with token and id.
       } ~ (path("update") & put) {
         formField(
           'token,
@@ -37,6 +43,7 @@ class TAchievementRoutes(tAchievementService: TAchievementService) {
         ) { (token, id, taType, date, description, msi, international) =>
             complete(tAchievementService.update(token, id, taType, date, description, msi, international))
           }
+        //add an achievement using valid token.
       } ~ (path("add") & post) {
         formField(
           'token,
